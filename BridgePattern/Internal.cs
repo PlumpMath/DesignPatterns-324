cd80cc33355b79948bae88ce898539a140d29b4c
Eddie Rangel <hacker@eddierangel.com>
2014-03-15 22:24:46 -0700

    Implemented Internal
    Created Public Properties and Print Methods

--- BridgePattern/Internal.cs
+++ BridgePattern/Internal.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace BridgePattern
 {
-    public class Internal
+    public class Internal : Referral
     {
+        public int InternalId { get; set; }
+        public Member Member { get; set; }
+
+        public string Symptoms { get; set; }
+
+        public Internal(IFormatter formatter)
+            : base(formatter)
+        {
+
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine(formatter.Format("Pharmacy Number", InternalId.ToString()));
+            Console.WriteLine(formatter.Format("Member Name", Member.FirstName + " " + Member.LastName));
+            Console.WriteLine(formatter.Format("Symptoms", Symptoms));
+            Console.WriteLine();
+        }
     }
 }
