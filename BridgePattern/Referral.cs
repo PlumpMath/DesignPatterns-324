29fd9f5fede1f29c5bc383da88a8ef52a1490da9
Eddie Rangel <hacker@eddierangel.com>
2014-03-15 22:00:18 -0700

    Creating Bridge Pattern Project
    Creating External, Internal, Pharmacy Classes to be used as Referrals.
    Created Abstract Referral Class. Created Interface IFormatter.

---
+++ BridgePattern/Referral.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgePattern
+{
+    public abstract class Referral
+    {
+
+        protected readonly IFormatter formatter;
+
+        public Referral(IFormatter formatter)
+        { 
+            this.formatter = formatter;
+        }
+
+        abstract public void Print();
+    }
+}
