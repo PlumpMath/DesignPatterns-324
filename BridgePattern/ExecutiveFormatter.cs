4bcd85f2fc7196f19778fabd62f21a41d0b7ddd3
Eddie Rangel <hacker@eddierangel.com>
2014-03-15 22:35:53 -0700

    Added Formatters
    Created Custom formatters to be used by Referrals

---
+++ BridgePattern/ExecutiveFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgePattern
+{
+    public class ExecutiveFormatter : IFormatter
+    {
+        public string Format(string key, string value)
+        {
+            return string.Format("<italics> {0} ----- =-- {1} <italics/>", key, value);
+        }
+    }
+}
